432a6833e7bdca0c293debabfea454c159c2273b
soknamu <sjw04093@naver.com>
2023-06-13 23:55:51 +0900

    Update excel생성기.py

--- keras/excel생성기.py
+++ keras/excel생성기.py
@@ -7,16 +7,7 @@ workbook = Workbook()
 sheet = workbook.active
 
 # 데이터 입력
-data = [
-    [1, 4, 15, 25, 35],
-    [2, 4, 15, 25, 35],
-    [3, 4, 15, 25, 35],
-    [4, 4, 15, 25, 35],
-    [5, 4, 15, 25, 35],
-    [6, 4, 15, 25, 35],
-    [7, 4, 15, 25, 35],
-    [8, 4, 15, 25, 35]
-]
+data = 
 
 for row in data:
     sheet.append(row)
@@ -30,3 +21,5 @@ for row in range(2, sheet.max_row + 1):
 
 # 엑셀 파일 저장
 workbook.save('./excel/유통기한_계산.xlsx')
+
+
